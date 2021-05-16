3b4f8d87c8d09dd22046391df675f94bd2774dc1
Manuel Urgell <54177784+manuelurgell@users.noreply.github.com>
2021-05-16 14:29:50 -0500

    Update models.py

--- companies/models.py
+++ companies/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from utils.models import ActiveMixin
 
 
-# Create your models here.
 class Company(ActiveMixin):
     id = models.CharField(max_length=4, primary_key=True)
     name = models.CharField(max_length=70)
